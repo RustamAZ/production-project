a3faa57dba9fc9a7c26699d45310e352262919e8
RustamAZ <Himynameisernie@yandex.ru>
2023-03-06 16:53:35 +0300

    Fix

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,14 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from 'shared/ui/Button/Button';
-import { ThemeButton } from './Button';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
-    test('test render', () => {
+    test('Test render', () => {
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
-    test('with theme', () => {
+    test('Test clear theme', () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
